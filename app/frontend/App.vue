9c3858316290eed7091670da7b5bab97be70a1ef
Ignacio321 <iediaz@uc.cl>
2026-07-24 14:39:24 -0400

    feat: add register form to home view and test

--- app/frontend/App.vue
+++ app/frontend/App.vue
@@ -1,19 +1,11 @@
 <script setup>
-import { ref } from 'vue'
-
-const count = ref(0)
+import RegisterForm from './components/RegisterForm.vue';
 </script>
 
 <template>
   <div
     class="min-h-screen flex flex-col items-center justify-center gap-4 bg-gray-100"
   >
-    <h1 class="text-3xl font-bold text-gray-800">Rails + Vue + Tailwind 🎉</h1>
-    <button
-      class="px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700"
-      @click="count++"
-    >
-      Clicks: {{ count }}
-    </button>
+    <RegisterForm/>
   </div>
 </template>
